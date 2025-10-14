69ec5bf1238b4f11e49130794f02484689adf45c
PriyankaSavani <savanipriyanka7396@gmail.com>
2025-10-14 16:15:45 -0400

    Fix import paths for page components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,12 +11,12 @@ const SignUp = React.lazy( () => import( '../pages/auth/SignUp' ) );
 const SignOut = React.lazy( () => import( '../pages/auth/SignOut' ) );
 
 // dashboard
-const Dashboard = React.lazy( () => import( '../pages/Dashboard/index.tsx' ) );
+const Dashboard = React.lazy( () => import( '../pages/Dashboard' ) );
 
 // timesheet management
-const Projects = React.lazy( () => import( '../pages/Projects/index.tsx' ) );
-const Timesheet = React.lazy( () => import( '../pages/Timesheet/index.tsx' ) );
-const Tasks = React.lazy( () => import( '../pages/Tasks/index.tsx' ) );
+const Projects = React.lazy( () => import( '../pages/Projects' ) );
+const Timesheet = React.lazy( () => import( '../pages/Timesheet' ) );
+const Tasks = React.lazy( () => import( '../pages/Tasks' ) );
 
 export interface RoutesProps {
      path: string;
